3b2df0c0172e7090a6371e38d0d505f49433f50a
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:21:16 +0530

    Create 1.py

---
+++ Week 2/1.py
@@ -0,0 +1,24 @@
+# Create a program that displays the name and complete mailing address as given in sample outputs.  Your program does not need to read any input from the user.
+
+
+
+# Sample Output:
+
+# B.Bhuvaneswaran
+
+# Department of Computer Science and Engineering
+
+# Rajalakshmi Engineering College
+
+# Thandalam
+
+# Chennai 602 105
+
+print("B.Bhuvaneswaran")
+print("Department of Computer Science and Engineering")
+
+print("Rajalakshmi Engineering College")
+
+print("Thandalam")
+
+print("Chennai 602 105")

f60b83b65329e7b2bb81230f7ddb5bf52a9627dd
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:37:39 +0530

    Create 2.py

---
+++ Week 5/2.py
@@ -0,0 +1,18 @@
+# Write a Python program to get one string and reverses a string. The input string is given as an array of characters char[].
+
+# You may assume all the characters consist of printable ascii characters.
+# Example 1:
+
+# Input: 
+# hello
+# Output: 
+# olleh
+# Example 2:
+
+# Input: 
+# Hannah
+# Output: 
+# hannaH
+
+s=input()
+print(s[::-1])

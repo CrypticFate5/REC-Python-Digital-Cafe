358e117d367c36935c9d305c38e5acce40399589
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:21:53 +0530

    Create 2.py

---
+++ Week 2/2.py
@@ -0,0 +1,12 @@
+# Write a program that asks the user to enter his or her name. The program should respond with a message that says hello to the user, using his or her name.
+
+# Sample Input 
+
+# Bhuvan
+
+# Sample Output
+
+# Hello Bhuvan
+
+a=input()
+print("Hello", a)

9fe3757b27282f868b8513dbfad37888bb427067
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:22:25 +0530

    Create 3.py

---
+++ Week 2/3.py
@@ -0,0 +1,15 @@
+# Write a program that asks the user to enter the width and length of a room. Once these values have been read, your program should compute and display the area of the room. The length and the width will be entered as floating-point numbers.
+
+# Sample Input
+
+# 11.11
+
+# 22.22
+
+# Sample Output
+
+# 246.86419999999998 square feet
+
+a=float(input())
+b=float(input())
+print(a*b,"square feet")

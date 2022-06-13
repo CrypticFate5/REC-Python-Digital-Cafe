f2d336194e8e3f212a822aa611962405e769d409
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:39:47 +0530

    Create 7.py

---
+++ Week 5/7.py
@@ -0,0 +1,38 @@
+# Write a code to reverse the case of a character input
+
+# Input Format:
+
+# Single character Input
+
+# Output Format:
+
+# Reversed character
+
+# Example Input:
+
+# R
+
+# Output:
+
+# r
+
+# Example Input:
+
+# a
+
+# Output:
+
+# A
+
+
+# For example:
+
+# Input	Result
+# R
+# r
+# a
+# A
+
+
+s=input()
+print(s.swapcase())

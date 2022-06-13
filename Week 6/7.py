1beeaecbddba4eb88f8d3f4a3080907bcba9054b
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:49:33 +0530

    Update 7.py

--- Week 6/7.py
+++ Week 6/7.py
@@ -1,1 +1,41 @@
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+# An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
 
+# Write a program if n array is monotonic or not. Print "True" if is monotonic or "False" if it is not. Array can be monotone increasing or decreasing
+
+# Sample Input1
+
+# 6578
+
+# Sample Output1
+
+# True
+
+# Sample Input2
+
+# 6543
+
+# Sample Output2
+
+# True
+
+# Sample Input 3
+
+# 6787
+
+# Sample Output3
+
+# False
+
+st=input()
+l=st.split()
+la=[]
+for i in range(0,len(l)-1):
+    for j in range(i+1,len(l)):
+        if(l[i]>=l[j] or l[i]<=l[j]):
+            la.append(0)
+            break
+if 1 in la:
+    print("False")
+else:
+    print("True")

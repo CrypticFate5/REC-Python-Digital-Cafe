36d0689f851e783198eda3875bfcfe8a20e74e64
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:45:30 +0530

    Create 6.py

---
+++ Week 6/6.py
@@ -0,0 +1,22 @@
+# Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print the company name of a given email address. Both user names and company names are composed of letters only.
+
+# Input Format:
+
+# The first line of the input contains an email address.
+
+# Output Format:
+
+# Print the company name in single line.
+
+# Example:
+
+# Input:
+
+# john@google.com
+
+# Output:
+
+# google
+
+st=input()
+print(st[st.index('@')+1:st.index('.')])

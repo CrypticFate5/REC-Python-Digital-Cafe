f0c521c426309bdb488b369cf5fecbfe06312423
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-13 23:36:43 +0530

    Create 10.py

---
+++ Week 4/10.py
@@ -0,0 +1,14 @@
+# Print the following pattern
+# Sample Input and Output
+# 5
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()

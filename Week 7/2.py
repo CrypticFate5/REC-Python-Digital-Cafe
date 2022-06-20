1810860c8f53beb41eb503df81640106ebc08de5
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-20 18:41:51 +0530

    Create 2.py

---
+++ Week 7/2.py
@@ -0,0 +1,18 @@
+# Create a tuple:
+
+# my_tuple = ('R','a','j','a','l','a','k','s','h','m','i')
+
+# and apply slicing and display the output as shown below:
+
+# ('R', 'a', 'j', 'a')
+# ('l', 'a', 'k', 's', 'h', 'm', 'i')
+# ('R', 'a', 'j')
+# ('l', 'a', 'k')
+# ('m', 'i')
+
+t=('R','a','j','a','l','a','k','s','h','m','i')
+print(t[0:4])
+print(t[4:])
+print(t[0:3])
+print(t[4:7])
+print(t[9:])

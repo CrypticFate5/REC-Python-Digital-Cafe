08123a8402c6a39a5c9c521376529dab488e0f11
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-20 18:41:29 +0530

    Create 1.py

---
+++ Week 7/1.py
@@ -0,0 +1,15 @@
+# Create different types of tuples as per below mentioned values and print the same.
+
+# ()
+# (1, 2, 3)
+# (1, 'REC', 3.4)
+# ('Python', [8, 4, 6], (1, 2, 3))
+
+t1=()
+t2=(1,2,3)
+t3=(1,'REC',3.4)
+t4=('Python',[8,4,6],t2)
+print(t1)
+print(t2)
+print(t3)
+print(t4)

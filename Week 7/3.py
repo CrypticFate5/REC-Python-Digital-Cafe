8a8729cc8984f03b3d0b50f1eb80a3104c21f8e6
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-20 18:42:11 +0530

    Create 3.py

---
+++ Week 7/3.py
@@ -0,0 +1,16 @@
+# Create a tuple t1 with numbers 1 to 5, t2 with 6 to 10 and t3 with a string "REC".
+
+# Concatenate t1 and t2 and print the result.
+
+# Repeat the t3 10 times without using any looping statements.
+
+# Expected output:
+
+# (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# ('REC', 'REC', 'REC', 'REC', 'REC', 'REC', 'REC', 'REC', 'REC', 'REC')
+
+t1=(1,2,3,4,5)
+t2=(6,7,8,9,10)
+t3=('REC',)
+print(t1+t2)
+print(t3*10)

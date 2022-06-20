79943cf4b0f4439c1198b44b585fb1d7739d93e0
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-20 18:42:50 +0530

    Create 5.py

---
+++ Week 7/5.py
@@ -0,0 +1,28 @@
+# Write a program to read a string and a character and find the whether the character is available in the string or not. Print True if the character is present in the string, False otherwise.
+
+# Sample Input
+
+# Rakalakshmi
+
+# a
+
+# Sample Output
+
+# True
+
+# Sample Input
+
+# Rakalakshmi
+
+# b
+
+# Sample Output
+
+# False
+
+st=input()
+ch=input()
+if ch in st:
+    print(True)
+else:
+    print(False)
